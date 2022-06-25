56a4900114597a8344c99bcdd70b1a65d3fd3265
Mr1cecream <guy@ochakov.com>
2022-06-25 22:04:33 +0300

    feat: add mod downloading

--- src/toml.rs
+++ src/toml.rs
@@ -1,16 +1,19 @@
 use crate::errors::BreezeError;
-use crate::{Loader, Mod, ModSide};
+use crate::{Mod, ModSide};
+use anyhow::Result;
+use libium::config::structs::ModLoader;
+use log::{info, warn};
 use serde::Deserialize;
 use std::collections::HashMap;
 
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct Pack {
-    name: String,
-    version: String,
-    loader: Loader,
-    mc_version: String,
-    mods: Vec<Mod>,
+    pub name: String,
+    pub version: String,
+    pub loader: ModLoader,
+    pub mc_version: String,
+    pub mods: Vec<Mod>,
 }
 
 #[allow(dead_code)]
@@ -31,33 +34,57 @@ struct Mods {
     all: HashMap<String, u32>,
 }
 
-pub fn parse(toml: String) -> Result<Pack, Box<dyn std::error::Error>> {
+/// Parse the given TOML string to a `Pack` struct format
+pub fn parse(toml: String) -> Result<Pack> {
     let data: Data = toml::from_str(toml.as_str())?;
-    let loader: Loader = match data.loader.to_lowercase().as_str() {
-        "forge" => Loader::Forge,
-        "fabric" => Loader::Fabric,
-        _ => return Err(Box::new(BreezeError::InvalidLoader)),
+
+    let loader: ModLoader = match data.loader.to_lowercase().as_str() {
+        "forge" => ModLoader::Forge,
+        "fabric" => ModLoader::Fabric,
+        _ => return Err(BreezeError::InvalidLoader.into()),
     };
+    info!("Found loader: {}", data.loader.to_lowercase());
 
     let mut mods: Vec<Mod> = Vec::new();
-
-    for id in data.mods.client.values() {
-        mods.push(Mod {
+    // has to be a better way to do this
+    for (name, id) in data.mods.client.iter() {
+        let mod_ = Mod {
+            name: name.to_string(),
             id: *id,
             side: ModSide::Client,
-        })
+        };
+        if mods.contains(&mod_) {
+            warn!("Found duplicate mod: {}, id: {}", name, id);
+            continue;
+        }
+        mods.push(mod_);
+        info!("Adding client mod: {}, id: {}", name, id);
     }
-    for id in data.mods.server.values() {
-        mods.push(Mod {
+    for (name, id) in data.mods.server.iter() {
+        let mod_ = Mod {
+            name: name.to_string(),
             id: *id,
             side: ModSide::Server,
-        })
+        };
+        if mods.contains(&mod_) {
+            warn!("Found duplicate mod: {}, id: {}", name, id);
+            continue;
+        }
+        mods.push(mod_);
+        info!("Adding server mod: {}, id: {}", name, id);
     }
-    for id in data.mods.all.values() {
-        mods.push(Mod {
+    for (name, id) in data.mods.all.iter() {
+        let mod_ = Mod {
+            name: name.to_string(),
             id: *id,
             side: ModSide::All,
-        })
+        };
+        if mods.contains(&mod_) {
+            warn!("Found duplicate mod: {}, id: {}", name, id);
+            continue;
+        }
+        mods.push(mod_);
+        info!("Adding general mod: {}, id: {}", name, id);
     }
 
     Ok(Pack {
