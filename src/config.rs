98c9d5f35502b8066aa40e9e8ff472d3341e34f9
Mr1cecream <guy@ochakov.com>
2022-11-11 15:09:07 +0200

    refactor: skip serializing for config if undefined

--- src/config.rs
+++ src/config.rs
@@ -6,11 +6,12 @@ use crate::ModSide;
 
 #[derive(Default, Serialize, Deserialize, Debug)]
 pub struct Config {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub mc_dir: Option<PathBuf>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub source: Option<PathOrUrl>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub side: Option<ModSide>,
-    pub resourcepacks: Option<bool>,
-    pub shaderpacks: Option<bool>,
 }
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
